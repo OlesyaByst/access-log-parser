5592fe04d823e83d390551fef691b8ae9fe6e725
Olesya Bystrushkina <testermpinvest@gmail.com>
2025-10-23 19:31:44 +0300

    Добавление логики вычисления чисел.

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,25 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>:" + Math.random());
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста:"+text.length());
+        System.out.println("Введите первое число:");
+        int number1 = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:");
+        int number2 = new Scanner(System.in).nextInt();
+
+        System.out.println("Сумма чисел:");
+        int sum=number1+number2;
+        System.out.println(sum);
+
+        System.out.println("Разность чисел:");
+        int difference= number1-number2;
+        System.out.println(difference);
+
+        System.out.println("Произведение чисел:");
+        int mult=number1*number2;
+        System.out.println(mult);
+
+        System.out.println("Частное чисел:");
+        double quotient=(double) number1/number2;
+        System.out.println(quotient);
     }
 }
