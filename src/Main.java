d25a8fae79ee297349360197abe74fe70a39ca47
Olesya Bystrushkina <testermpinvest@gmail.com>
2025-12-08 19:33:22 +0300

    задача 3

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,16 @@
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class Main {
 
 
     private static final int maxLenghtLine = 1024;
+    private static final Pattern FIRST_BRACKETS_PATTERN = Pattern.compile("\\((.*?)\\)");
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -37,24 +39,29 @@ public class Main {
         int lineCount = 0; //текст строки
         String minLength = null;
         String maxLength = null;//символы в строке
-        List<String> all = new ArrayList<>();
+        List<String> allLines = new ArrayList<>(); //хранение всех строк
         try (
                     FileReader fileReader = new FileReader(path);
                     BufferedReader reader = new BufferedReader(fileReader)
             ) {
-                String line = ""; //содержимое строки
-                String firstBrackets = Arrays.toString("\\((.*?)\\)".split(line)); // взяли часть между скобок
-            List<String> fragment = new ArrayList<>();
+                String line; //содержимое строки
+            List<String> fragment1 = new ArrayList<>();
+            List<String> fragment2 = new ArrayList<>();
+
                 while ((line = reader.readLine()) != null) {
-                    all.add(line);//сами строки
+                    allLines.add(line);//сами строки
                     lineCount++;
-                    String[] parts = firstBrackets.split(";"); //разделили по точке с запятой
-                    if (parts.length >= 2) {
-                         fragment.add(parts[0].trim());//добавляем фрагменты в массив и удаляем пробелы
-                         fragment.add(parts[1].trim().split("/")); // часть до слеша без пробелов
+                    Matcher matcher = FIRST_BRACKETS_PATTERN.matcher(line); //хранит текст
+                    if (matcher.find()) {
+                        String contentInsideBrackets = matcher.group(1); // 1. Извлекаем текст из скобок
+                        String[] parts = contentInsideBrackets.split(";");  // текст до ;
+
+                        if (parts.length >= 2) {
+                            fragment1.add(parts[0].trim()); // левая - первая часть
+                            String botName = parts[0].trim().split("/")[0]; // правая-вторая часть
+                            fragment2.add(botName); // Результат: "SemrushBot"
+                        }
                     }
-
-
                     if (minLength == null || line.length() < minLength.length()) {
                         minLength = line;
                     }
@@ -65,9 +72,9 @@ public class Main {
                         throw new RuntimeException("В файле присутствует строка длиннее 1024 символов ");
                     }
                 }
-            String finalMaxLength = maxLength;
-            List<String> filteredLines = all.stream()
-                    .filter(line -> !line.equals(minLength) && !line.equals(maxLength)).collect(Collectors.toList());
+            List<String> filteredLines = new ArrayList<>(allLines);
+                 filteredLines.remove(minLength);
+                 filteredLines.remove(maxLength);
             } catch(RuntimeException e){
                     System.err.println("Ошибка строки");
                     e.printStackTrace();
